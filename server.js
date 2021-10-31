1076ae2240560083cffc49b285fee8e3bb4c8b79
Margarita555 <a_rita@ukr.net>
2021-10-31 22:50:58 +0200

    +

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
   static: './build',
 });
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 4000;
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
--- src/helpers/Constants.jsx
+++ src/helpers/Constants.jsx
@@ -1,4 +1,5 @@
 // export const JSON_API = 'https://react-todo-list-test.herokuapp.com/api'
-export const JSON_API = 'https://github.com/Margarita555/react-test-todo2-main-db.json/blob/[main|master]/db.json'
+
+export const JSON_API = 'https://my-json-server.typicode.com/margarita555/react-test-todo2'
 
 // export const JSON_API = 'http://localhost:3004'
