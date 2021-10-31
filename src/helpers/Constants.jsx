82220df7e86aebeb442c7312d0fb3fc2fa5c4c04
Margarita555 <a_rita@ukr.net>
2021-10-31 21:59:58 +0200

    +

--- src/helpers/Constants.jsx
+++ src/helpers/Constants.jsx
@@ -1,3 +1,4 @@
-export const JSON_API = 'https://react-todo-list-test.herokuapp.com/api'
+// export const JSON_API = 'https://react-todo-list-test.herokuapp.com/api'
+export const JSON_API = 'https://github.com/Margarita555/react-test-todo2-main-db.json/blob/[main|master]/db.json'
 
 // export const JSON_API = 'http://localhost:3004'
