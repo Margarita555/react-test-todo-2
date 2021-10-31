1cca82892e82c30d4f624d61a4b1bd2775cfb6c6
Margarita555 <a_rita@ukr.net>
2021-10-31 23:39:37 +0200

    +

--- src/components/My-photoOLD.jsx
+++
@@ -1,7 +0,0 @@
-import photo from '../images/my-photo.jpg';
-import {AppPhoto} from './Header/Header.module.css'
-
-const MyPhoto = () => {
-    return <img src={photo} className={AppPhoto} alt="it is me" />
-}
-export default MyPhoto;
