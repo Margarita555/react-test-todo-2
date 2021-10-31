36150b645580f1f834e818bc220c98148fbb2015
Margarita555 <a_rita@ukr.net>
2021-10-31 23:14:49 +0200

    Update Constants.jsx

--- src/helpers/Constants.jsx
+++ src/helpers/Constants.jsx
@@ -1,5 +1,5 @@
 // export const JSON_API = 'https://react-todo-list-test.herokuapp.com/api'
 
-export const JSON_API = 'https://my-json-server.typicode.com/margarita555/react-test-todo2'
+export const JSON_API = 'https://my-json-server.typicode.com/margarita555/react-test-todo-2'
 
 // export const JSON_API = 'http://localhost:3004'
